780d6bfd899fbf39f4b95c63f94524d0376eedde
recontemplator <recontemplator@gmail.com>
2016-12-30 00:53:36 +0300

    Single/double quotes bug fixed. Rid of debug console output.

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -102,9 +102,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       var categoryObject = chooseRandomCategory(categories);
-      console.log(categoryObject);
-      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
-      console.log(chosenCategoryShortName);
+      var chosenCategoryShortName = "'"+categoryObject.short_name+"'";
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -117,15 +115,13 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      var homeHtmlToInsertIntoMainPage =insertProperty(homeHtml,'randomCategoryShortName','"'+chosenCategoryShortName+'"');
-      console.log(homeHtmlToInsertIntoMainPage);
+      var homeHtmlToInsertIntoMainPage =insertProperty(homeHtml,'randomCategoryShortName',chosenCategoryShortName);
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
-      console.log('done');
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
@@ -156,7 +152,7 @@ dc.loadMenuCategories = function () {
 dc.loadMenuItems = function (categoryShort) {
   showLoading("#main-content");
   $ajaxUtils.sendGetRequest(
-    menuItemsUrl + categoryShort,
+    menuItemsUrl + $.trim(categoryShort),
     buildAndShowMenuItemsHTML);
 };
 
